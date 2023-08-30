dfb0ada581abf4587a9dda94b4a1262ec21bcf79
fazal-10 <fazalmansuri08@gmail.com>
2023-08-30 07:42:17 +0530

    wrote first test case in test.spec.js file

--- test.spec.js
+++ test.spec.js
@@ -1,5 +1,11 @@
 import {LunarCraft} from './index';
 
-const craft = new LunarCraft();
-const commands = ["f"];
-craft.executeCommands(commands);
+describe("LunarCraft", () => {
+    test('Executing ["f"] command should move spacecraft forward', () => {
+        const craft = new LunarCraft();
+        const commands = ["f"];
+        craft.executeCommands(commands);
+        expect(craft.getPosition()).toEqual({ x: 0, y: 1, z: 0 });
+        expect(craft.getDirection()).toBe("N");
+    });
+});
